9e1f751d5e27623c2af7715c0fb9736e5a923f0d
DanielRavina <danielravina@gmail.com>
2016-04-23 11:27:07 -0700

    comment on sugar

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ var state = {
   eat: false,
 }
 
-var questions = {
+var conversation = {
   check: [
     'how much your sugar?',
     'did you check sugar?',
@@ -24,11 +24,38 @@ var questions = {
   inject: [
     'did you inject?',
     'inject??',
-    'injection?'
+    'injection?',
+    'will you inject?'
   ],
   eat: [
     'what did you eat?',
-    'eat??'
+    'food??',
+  ],
+  goodSugar: [
+    'Nice! :100:',
+    'Good job! :+1:',
+    'Mimi will be proud :heart: :simple_smile:',
+    ':ok_hand:',
+    ':clap:'
+  ],
+  highSugar: [
+    ':pouting_cat:',
+    'you had too much :cake:',
+    'Please Inject! :syringe:',
+    ':face_with_head_bandage: Not very good...',
+    'Not good :-1:',
+    'high sugar :baby_bottle:',
+  ],
+  lowSugar: [
+    'low sugar :baby_bottle:',
+    'You need to eat :apple: and then :bread:',
+    'Too low!',
+    ':chocolate_bar:'
+  ],
+  thanks: [
+    'Thank you :wave:',
+    'talk later',
+    ':+1:'
   ]
 }
 
@@ -36,8 +63,8 @@ mongoose.connect(db)
 
 var today;
 var PATIENT  = 'U135E4DG9'
-var CHANNEL = 'C1357PUCB'
-var token   = process.env.DIABOTESE_TOKEN
+var CHANNEL = 'D1359F8H3'//'C1357PUCB'
+var token   = 'xoxb-37178394679-cNKJVTSDRmL2O7cU18cF7Rsk'// process.env.DIABOTESE_TOKEN
 
 var rtm = new RtmClient(token);
 rtm.start();
@@ -45,32 +72,34 @@ rtm.start();
 rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
   setTimeout(function(){
     if(isTimeToCheck()) {
-      didYouCheckSugar()
     }
   },60000)
+  didYouCheckSugar()
 })
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
+  console.log(message)
   if (message.user != PATIENT) return
 
   if (state.check) {
     today.checks.push({ unit: message.text, time: Date.now() })
+    commentOnSugar(message.text)
     today.save()
-    rtm.sendMessage(sample(questions.inject), CHANNEL, function() {
+    rtm.sendMessage(sample(conversation.inject), CHANNEL, function() {
       setState('inject')
     });
 
   } else if(state.inject) {
     today.injections.push({ unit: message.text, time: Date.now() })
     today.save()
-    rtm.sendMessage(sample(questions.eat), CHANNEL, function() {
+    rtm.sendMessage(sample(conversation.eat), CHANNEL, function() {
       setState('eat')
     });
 
   } else if(state.eat) {
     today.food.push({ description: message.text, time: Date.now() })
     today.save()
-    rtm.sendMessage('Thank you', CHANNEL, function() {
+    rtm.sendMessage(sample(conversation.thanks), CHANNEL, function() {
       setState('eat')
     });
     resetState()
@@ -82,8 +111,21 @@ function isTimeToCheck() {
   return ['0800','1200', '1600', '1800', '2300'].indexOf(hhmm) !== -1
 }
 
+function commentOnSugar(value) {
+  value = Number.parseFloat(value)
+  var message;
+  if(value < 5) {
+    message = sample(conversation.lowSugar)
+  } else if(value < 9) {
+    message = sample(conversation.goodSugar)
+  } else {
+    message = sample(conversation.highSugar)
+  }
+  rtm.sendMessage(message, CHANNEL)
+}
+
 function didYouCheckSugar() {
-  rtm.sendMessage(sample(questions.check), CHANNEL, function() {
+  rtm.sendMessage(sample(conversation.check), CHANNEL, function() {
     setState('check')
     askedSugar = true
     var day = moment().format('l');  // 4/22/2016
